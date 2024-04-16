4774a39f5a294569dbc44e56d804ca5f2413f725
Ana Loureiro <ana.wk.lsl@gmail.com>
2024-04-16 05:05:28 +0100

    reservar sala

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-}
-
-dependencies {
-    implementation 'com.google.android.material:material:<latest_version>'
-}
